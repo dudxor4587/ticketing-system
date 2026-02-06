741c98ea5890fe26a1cc13eb2a84e2ae693b659c
dudxor4587 <dudxor4587@gmail.com>
2026-02-06 14:33:15 +0900

    feat: 프로젝트 의존성 추가

--- src/main/java/com/ticketing/TicketingApplication.java
+++ src/main/java/com/ticketing/TicketingApplication.java
@@ -1,13 +1,12 @@
-package com.ticketingsystem.ticketingsystem;
+package com.ticketing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketingSystemApplication {
+public class TicketingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketingSystemApplication.class, args);
+        SpringApplication.run(TicketingApplication.class, args);
     }
-
 }
--- src/test/java/com/ticketing/TicketingApplicationTests.java
+++ src/test/java/com/ticketing/TicketingApplicationTests.java
@@ -1,13 +1,12 @@
-package com.ticketingsystem.ticketingsystem;
+package com.ticketing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TicketingSystemApplicationTests {
+class TicketingApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
